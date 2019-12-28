2a983418394172168cfc635a4333a9e2c21d2e25
Michael Hienle <michael.hienle@tngtech.com>
2019-12-28 12:42:02 +0100

    Initially set the sidebar as open

--- demo/src/Layout.tsx
+++ demo/src/Layout.tsx
@@ -92,7 +92,7 @@ const useStyles = makeStyles(theme =>
 export default function Layout() {
   const classes = useStyles();
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
 
   const handleDrawerOpen = () => {
     setOpen(true);
