91727f0d7762fdaa3431b163d13f36a2cb8ca1f3
Michael Hienle <michael.hienle@tngtech.com>
2020-01-17 14:25:38 +0100

    Fix the version deployed on Netlify
    Did not work after hard reload if not on route /

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Layout from "./Layout";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Layout />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
